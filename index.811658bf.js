dca0fa69954876b928232c6c1fce597d54e7ab33
MaksymDpUa <117561870+MaksymDpUa@users.noreply.github.com>
2023-05-14 10:31:16 +0000

    Deploying to gh-pages from @ MaksymDpUa/test@3440c8cf2c56db9272d589ead70e7e777bc7ed34 🚀

---
+++ index.811658bf.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{direction:"vertical",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.811658bf.js.map
